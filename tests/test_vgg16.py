21243928bc517815d702a4195ae7be6811792275
ryo.ishii <ryoishii1101@gmail.com>
2020-05-19 23:22:28 +0900

    Update: 入力の形修正, 軽微な修正

--- tests/test_vgg16.py
+++ tests/test_vgg16.py
@@ -7,11 +7,10 @@ def test_block_cbr():
     filters = 64
     kernel_size = 3
     strides = 2
-    input_shape = (10, 256, 256, 3)
+    input_shape = (10, 224, 224, 3)
     inputs = tf.zeros((input_shape), dtype=tf.float32)
     model = BlockCBR(filters, kernel_size, strides)
 
-    model.build_graph(input_shape)
     assert model(inputs).shape == (
         input_shape[0],
         input_shape[1] // strides,
@@ -19,15 +18,18 @@ def test_block_cbr():
         filters,
     )
 
+    model.build_graph(input_shape)
     model.summary()
 
 
 def test_vgg16():
-    input_shape = (10, 256, 256, 3)
+    input_shape = (10, 224, 224, 3)
     inputs = tf.zeros((input_shape), dtype=tf.float32)
-    output_shape = 10
+    output_size = 1000
+    output_shape = (10, output_size)
 
-    model = VGG16(output_shape)
+    model = VGG16(output_size)
+    assert model(inputs).shape == output_shape
+
     model.build_graph(input_shape)
-    model(inputs).shape == output_shape
     model.summary()
