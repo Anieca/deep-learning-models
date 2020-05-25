ee808d8f29a3053e205f2f5009a4bd6f20f568b0
ryo.ishii <ryoishii1101@gmail.com>
2020-05-26 00:18:57 +0900

    Update: functional API の ResNet50, Subclassing API のResNet101 実装

--- src/resnet50.py
+++ src/resnet50.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, Model
 
 
-class BottleneckResBlock(Model):
+class BottleneckResidual(Model):
     """ResNet の Bottleneck Block です.
     1 層目の 1x1 conv で ch 次元を削減することで
     2 層目の 3x3 conv の計算量を減らし
@@ -99,26 +99,26 @@ class ResNet50(Model):
         """
         super().__init__(*args, **kwargs)
 
-        self._layers = [
+        self.layers_ = [
             layers.Conv2D(64, 7, 2, padding="same", kernel_initializer="he_normal"),
             layers.BatchNormalization(),
             layers.MaxPool2D(pool_size=3, strides=2, padding="same"),
-            BottleneckResBlock(64, 256),
-            BottleneckResBlock(256, 256),
-            BottleneckResBlock(256, 256),
-            BottleneckResBlock(256, 512, 2),
-            BottleneckResBlock(512, 512),
-            BottleneckResBlock(512, 512),
-            BottleneckResBlock(512, 512),
-            BottleneckResBlock(512, 1024, 2),
-            BottleneckResBlock(1024, 1024),
-            BottleneckResBlock(1024, 1024),
-            BottleneckResBlock(1024, 1024),
-            BottleneckResBlock(1024, 1024),
-            BottleneckResBlock(1024, 1024),
-            BottleneckResBlock(1024, 2048, 2),
-            BottleneckResBlock(2048, 2048),
-            BottleneckResBlock(2048, 2048),
+            BottleneckResidual(64, 256),
+            BottleneckResidual(256, 256),
+            BottleneckResidual(256, 256),
+            BottleneckResidual(256, 512, 2),
+            BottleneckResidual(512, 512),
+            BottleneckResidual(512, 512),
+            BottleneckResidual(512, 512),
+            BottleneckResidual(512, 1024, 2),
+            BottleneckResidual(1024, 1024),
+            BottleneckResidual(1024, 1024),
+            BottleneckResidual(1024, 1024),
+            BottleneckResidual(1024, 1024),
+            BottleneckResidual(1024, 1024),
+            BottleneckResidual(1024, 2048, 2),
+            BottleneckResidual(2048, 2048),
+            BottleneckResidual(2048, 2048),
             layers.GlobalAveragePooling2D(),
             layers.Dense(
                 output_size, activation="softmax", kernel_initializer="he_normal"
@@ -132,7 +132,7 @@ class ResNet50(Model):
         Returns:
             inputs(tf.Tensor):
         """
-        for layer in self._layers:
+        for layer in self.layers_:
             inputs = layer(inputs)
         return inputs
 
@@ -145,3 +145,118 @@ class ResNet50(Model):
         self.build(input_shape)
         inputs = tf.keras.Input(shape=input_shape_nobatch)
         self.call(inputs)
+
+
+def functional_bottleneck_residual(x, in_ch, out_ch, strides=1):
+    params = {
+        "padding": "same",
+        "kernel_initializer": "he_normal",
+        "use_bias": True,
+    }
+    inter_ch = out_ch // 4
+    h1 = layers.Conv2D(inter_ch, kernel_size=1, strides=strides, **params)(x)
+    h1 = layers.BatchNormalization()(h1)
+    h1 = layers.ReLU()(h1)
+    h1 = layers.Conv2D(inter_ch, kernel_size=3, strides=1, **params)(h1)
+    h1 = layers.BatchNormalization()(h1)
+    h1 = layers.ReLU()(h1)
+    h1 = layers.Conv2D(out_ch, kernel_size=1, strides=1, **params)(h1)
+    h1 = layers.BatchNormalization()(h1)
+
+    if in_ch != out_ch:
+        h2 = layers.Conv2D(out_ch, kernel_size=1, strides=strides, **params)(x)
+        h2 = layers.BatchNormalization()(h2)
+    else:
+        h2 = x
+
+    h = layers.Add()([h1, h2])
+    h = layers.ReLU()(h)
+    return h
+
+
+def functional_resnet50(input_shape, output_size):
+    inputs = layers.Input(batch_input_shape=input_shape)
+    x = layers.Conv2D(64, 7, 2, padding="same", kernel_initializer="he_normal")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=3, strides=2, padding="same")(x)
+
+    x = functional_bottleneck_residual(x, 64, 256)
+    x = functional_bottleneck_residual(x, 256, 256)
+    x = functional_bottleneck_residual(x, 256, 256)
+
+    x = functional_bottleneck_residual(x, 256, 512, 2)
+    x = functional_bottleneck_residual(x, 512, 512)
+    x = functional_bottleneck_residual(x, 512, 512)
+    x = functional_bottleneck_residual(x, 512, 512)
+
+    x = functional_bottleneck_residual(x, 512, 1024, 2)
+    x = functional_bottleneck_residual(x, 1024, 1024)
+    x = functional_bottleneck_residual(x, 1024, 1024)
+    x = functional_bottleneck_residual(x, 1024, 1024)
+    x = functional_bottleneck_residual(x, 1024, 1024)
+    x = functional_bottleneck_residual(x, 1024, 1024)
+
+    x = functional_bottleneck_residual(x, 1024, 2048, 2)
+    x = functional_bottleneck_residual(x, 2048, 2048)
+    x = functional_bottleneck_residual(x, 2048, 2048)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    outputs = layers.Dense(
+        output_size, activation="softmax", kernel_initializer="he_normal"
+    )(x)
+    return Model(inputs=inputs, outputs=outputs)
+
+
+class ResNet101(ResNet50):
+    """ResNet101 です.
+    要素は
+    conv * 1
+    resblock(conv * 3) * 3
+    resblock(conv * 3) * 4
+    resblock(conv * 3) * 23
+    resblock(conv * 3) * 3
+    dense * 1
+    から構成されていて, conv * 101 + dense * 1 の 101 層です.
+    """
+
+    def __init__(self, output_size=1000, *args, **kwargs):
+        """
+        Args:
+            output_size(int): num of class
+        """
+        super().__init__(*args, **kwargs)
+
+        self.layers_ = [
+            layers.Conv2D(64, 7, 2, padding="same", kernel_initializer="he_normal"),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=3, strides=2, padding="same"),
+        ]
+        out_ch = 256
+        for i in range(3):
+            in_ch = out_ch // 4 if i == 0 else out_ch
+            self.layers_.append(BottleneckResidual(in_ch, out_ch))
+
+        out_ch = 512
+        for i in range(4):
+            in_ch = out_ch // 4 if i == 0 else out_ch
+            strides = 2 if i == 0 else 1
+            self.layers_.append(BottleneckResidual(in_ch, out_ch, strides))
+
+        out_ch = 1024
+        for i in range(23):
+            in_ch = out_ch // 4 if i == 0 else out_ch
+            strides = 2 if i == 0 else 1
+            self.layers_.append(BottleneckResidual(in_ch, out_ch, strides))
+
+        out_ch = 2048
+        for i in range(3):
+            in_ch = out_ch // 4 if i == 0 else out_ch
+            strides = 2 if i == 0 else 1
+            self.layers_.append(BottleneckResidual(in_ch, out_ch, strides))
+
+        self.layers_ += [
+            layers.GlobalAveragePooling2D(),
+            layers.Dense(
+                output_size, activation="softmax", kernel_initializer="he_normal"
+            ),
+        ]
--- tests/test_resnet50.py
+++ tests/test_resnet50.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from src.resnet50 import BottleneckResBlock, ResNet50
+from src.resnet50 import BottleneckResidual, ResNet50, ResNet101
+from src.resnet50 import functional_resnet50
 
 
 def test_bottlenect_res_block_not_projection():
@@ -7,7 +8,7 @@ def test_bottlenect_res_block_not_projection():
     inputs = tf.zeros((input_shape), dtype=tf.float32)
     output_shape = (10, 224, 224, 256)
 
-    model = BottleneckResBlock(256, 256)
+    model = BottleneckResidual(256, 256)
     assert model(inputs).shape == output_shape
     model.build_graph(input_shape)
     model.summary()
@@ -18,19 +19,41 @@ def test_bottlenect_res_block_projection():
     inputs = tf.zeros((input_shape), dtype=tf.float32)
     output_shape = (10, 224, 224, 256)
 
-    model = BottleneckResBlock(64, 256)
+    model = BottleneckResidual(64, 256)
     assert model(inputs).shape == output_shape
     model.build_graph(input_shape)
     model.summary()
 
 
-def test_resnet():
+def test_resnet50():
     input_shape = (10, 224, 224, 3)
     inputs = tf.zeros((input_shape), dtype=tf.float32)
     output_size = 1000
     output_shape = (10, output_size)
 
     model = ResNet50(output_size)
+    assert model(inputs).shape == output_shape
+    model.build_graph(input_shape)
+    model.summary()
+
+
+def test_functional_resnet50():
+    input_shape = (10, 224, 224, 3)
+    inputs = tf.keras.layers.Input(batch_input_shape=input_shape)
+    output_size = 1000
+    output_shape = (10, output_size)
+    model = functional_resnet50(input_shape, output_size)
+    model.summary()
+    assert model(inputs).shape == output_shape
+
+
+def test_resnet101():
+    input_shape = (10, 224, 224, 3)
+    inputs = tf.zeros((input_shape), dtype=tf.float32)
+    output_size = 1000
+    output_shape = (10, output_size)
+
+    model = ResNet101(output_size)
     assert model(inputs).shape == output_shape
     model.build_graph(input_shape)
     model.summary()
