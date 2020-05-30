df8b42d6c92394c83cffe63acb61f64e577c1329
ryo.ishii <ryoishii1101@gmail.com>
2020-05-30 14:08:47 +0900

    Update: tf.function decorator, bug fix

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import math
 import os
 import tensorflow as tf
 
 from src.utils import load_dataset, load_model, get_args, get_current_time, augment
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+tf.random.set_seed(0)
 
 
 def builtin_train(args):
@@ -21,22 +23,32 @@ def builtin_train(args):
     # 3. loss, optimizer, metrics setting
     model.compile(
         optimizer=tf.keras.optimizers.Adam(),
-        loss="sparse_categorical_crossentropy",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=["accuracy"],
     )
 
-    # 4. dataset config (and validation, callback config)
+    # 4. dataset config
+    buffer_size = len(train_images)
+    train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    train_ds = train_ds.shuffle(buffer_size)
+    if args.augmentation:
+        train_ds = train_ds.map(augment)
+    train_ds = train_ds.batch(args.batch_size)
+    test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+    test_ds = test_ds.batch(args.batch_size)
+
     fit_params = {}
     fit_params["batch_size"] = args.batch_size
     fit_params["epochs"] = args.max_epoch
     if args.steps_per_epoch:
         fit_params["steps_per_epoch"] = args.steps_per_epoch
     fit_params["verbose"] = 1
+    fit_params["shuffle"] = True
     fit_params["callbacks"] = [tensorboard_callback]
-    fit_params["validation_data"] = (test_images, test_labels)
+    fit_params["validation_data"] = test_ds
 
     # 5. start train and test
-    model.fit(train_images, train_labels, **fit_params)
+    model.fit(train_ds, **fit_params)
 
 
 def custom_train(args):
@@ -53,8 +65,8 @@ def custom_train(args):
     test_writer = tf.summary.create_file_writer(os.path.join(logdir, "test"))
 
     # 3. loss, optimizer, metrics setting
-    criterion = tf.keras.losses.SparseCategoricalCrossentropy()
     optimizer = tf.keras.optimizers.Adam()
+    criterion = tf.keras.losses.SparseCategoricalCrossentropy()
     train_loss_avg = tf.keras.metrics.Mean()
     train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
     test_loss_avg = tf.keras.metrics.Mean()
@@ -62,9 +74,10 @@ def custom_train(args):
 
     # 4. dataset config
     buffer_size = len(train_images)
-    steps_per_epoch = (
-        args.steps_per_epoch if args.steps_per_epoch else buffer_size // args.batch_size
-    )
+    train_steps_per_epoch = math.ceil(len(train_images) / args.batch_size)
+    if args.steps_per_epoch:
+        train_steps_per_epoch = min(args.steps_per_epoch, train_steps_per_epoch)
+    test_steps_per_epoch = math.ceil(len(test_images) / args.batch_size)
 
     train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
     train_ds = train_ds.shuffle(buffer_size)
@@ -75,62 +88,47 @@ def custom_train(args):
     test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
     test_ds = test_ds.batch(args.batch_size)
 
+    @tf.function
+    def train_step(x, y_true):
+        with tf.GradientTape() as tape:
+            y_pred = model(x, training=True)
+            loss = criterion(y_true, y_pred)
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        avg_loss = train_loss_avg(loss)
+        avg_acc = train_accuracy(y_true, y_pred)
+        return avg_loss, avg_acc
+
+    @tf.function
+    def test_step(x, y_true):
+        y_pred = model(x, training=False)
+        loss = criterion(y_true, y_pred)
+        avg_loss = test_loss_avg(loss)
+        avg_acc = test_accuracy(y_true, y_pred)
+        return avg_loss, avg_acc
+
     # 5. start train and test
     for epoch in range(args.max_epoch):
-        # 5.1. initialize metrics
+        # 5.1. initialize metrics and progress bar
         train_loss_avg.reset_states()
         train_accuracy.reset_states()
         test_loss_avg.reset_states()
-        test_loss_avg.reset_states()
+        test_accuracy.reset_states()
 
-        # 5.2. initialize progress bar
-        train_pbar = tf.keras.utils.Progbar(steps_per_epoch)
-        test_pbar = tf.keras.utils.Progbar(steps_per_epoch)
+        train_pbar = tf.keras.utils.Progbar(train_steps_per_epoch)
+        test_pbar = tf.keras.utils.Progbar(test_steps_per_epoch)
 
-        # 5.3. start train
+        # 5.3. train
         for i, (x, y_true) in enumerate(train_ds):
-            if i >= steps_per_epoch:
+            if i >= train_steps_per_epoch:
                 break
-            # 5.3.1. forward
-            with tf.GradientTape() as tape:
-                y_pred = model(x)
-                loss = criterion(y_true=y_true, y_pred=y_pred)
+            loss, acc = train_step(x, y_true)
+            train_pbar.update(i + 1, [("loss", loss), ("accuracy", acc)])
 
-            # 5.3.2. calculate gradients from `tape` and backward
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            gradient_param = sum(sum(sum(sum(gradients[0]))))
-
-            # 5.3.3. update metrics and progress bar
-            train_loss_avg(loss)
-            train_accuracy(y_true, y_pred)
-            train_pbar.update(
-                i + 1,
-                [
-                    ("avg_loss", train_loss_avg.result()),
-                    ("accuracy", train_accuracy.result()),
-                    ("gradient", gradient_param.numpy()),
-                ],
-            )
-
-        # 5.4. start test
+        # 5.4. test
         for i, (x, y_true) in enumerate(test_ds):
-            if i >= steps_per_epoch:
-                break
-            # 5.4.1. forward
-            y_pred = model(x)
-            loss = criterion(y_true, y_pred)
-
-            # 5.4.2. update metrics and progress bar
-            test_loss_avg(loss)
-            test_accuracy(y_true, y_pred)
-            test_pbar.update(
-                i + 1,
-                [
-                    ("avg_test_loss", test_loss_avg.result()),
-                    ("test_accuracy", test_accuracy.result()),
-                ],
-            )
+            loss, acc = test_step(x, y_true)
+            test_pbar.update(i + 1, [("test_loss", loss), ("test_accuracy", acc)])
 
         # 5.5. write metrics to tensorboard
         with train_writer.as_default():
